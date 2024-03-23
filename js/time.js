45e9bdfb3ffce1f34a989df4dcfaf5e8685225e8
Sye <119392569+Sye0001@users.noreply.github.com>
2024-03-23 23:58:14 +1000

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,21 +1,17 @@
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+fetch("https://worldtimeapi.org/api/timezone/" + userTimeZone)
+    .then(response => response.json())
+    .then(data => {
 
-  fetch("http://worldtimeapi.org/api/timezone/" + userTimeZone)
-      .then(response => response.json())
-      .then(data => {
+        const datetime = data.datetime;
 
-          const datetime = data.datetime;
-          
+        const userDate = new Date(datetime);
 
-          const userDate = new Date(datetime);
-          
+        const formattedTime = userDate.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
 
-          const formattedTime = userDate.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
-          
-
-          document.getElementById('user-time').innerText = formattedTime;
-      })
-      .catch(error => {
-          console.error('Error fetching time:', error);
-      });
+        document.getElementById('user-time').innerText = formattedTime;
+    })
+    .catch(error => {
+        console.error('Error fetching time:', error);
+    });
