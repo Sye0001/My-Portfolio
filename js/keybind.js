8b3f545f34884d98ae4ef777b338b285eb32db2b
Sye <119392569+Sye0001@users.noreply.github.com>
2024-03-24 18:58:22 +1000

    Delete js directory

--- js/keybind.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.key === "1") {
-        window.location.href = './index.html';
-    } else if (event.key === "2") {
-        window.location.href = './work.html';
-    } else if (event.key === "3") {
-        window.location.href = './stack.html';
-    }
-});
