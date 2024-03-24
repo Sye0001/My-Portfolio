c54b6fe840092d226c999d3493ec2b56cc7f2d15
Sye <119392569+Sye0001@users.noreply.github.com>
2024-03-24 18:59:32 +1000

    Add files via upload

---
+++ js/time.js
@@ -0,0 +1,21 @@
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+function fetchAndDisplayTime() {
+    fetch("https://worldtimeapi.org/api/timezone/" + userTimeZone)
+        .then(response => response.json())
+        .then(data => {
+            const datetime = data.datetime;
+            const userDate = new Date(datetime);
+            const formattedTime = userDate.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
+            document.getElementById('user-time').innerText = formattedTime;
+        })
+        .catch(error => {
+            console.error('Error fetching time:', error);
+        });
+}
+
+// Initial fetch
+fetchAndDisplayTime();
+
+// Fetch time every 30 seconds
+setInterval(fetchAndDisplayTime, 30 * 1000); // 30 seconds in milliseconds
