2f9ea97f0a031241e7b9861ab23b35eceb0dfbb6
Sye <119392569+Sye0001@users.noreply.github.com>
2024-03-25 13:06:49 +1000

    Update keybind.js

--- js/keybind.js
+++ js/keybind.js
@@ -2,7 +2,7 @@ document.addEventListener('keydown', function(event) {
     if (event.key === "1") {
         window.location.href = './index.html';
     } else if (event.key === "2") {
-        window.location.href = './work.html';
+        window.location.href = './work/discord-clone.html';
     } else if (event.key === "3") {
         window.location.href = './stack.html';
     }
